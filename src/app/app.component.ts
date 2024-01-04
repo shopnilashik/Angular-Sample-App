da46db6a857ebaeba969830f1aa85b1c27dd4e14
shopnilashik <shopnil.ashik@gmail.com>
2024-01-04 19:37:12 +0600

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,6 +10,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatInputModule} from '@angular/material/input';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
@@ -25,18 +27,24 @@ import {MatInputModule} from '@angular/material/input';
     MatSelectModule,
     MatButtonModule,
     MatInputModule,
-    // BrowserModule,
-    // BrowserAnimationsModule
-    // NoopAnimationsModule
+    HomeComponent,
+    AboutComponent,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   // animations: [myNg]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
   showFiller= true;
-
+  
+  ngOnInit(): void {
+    
+  }
+  ll : string = "Hellllllllll"
   showFillerTest() {
     console.log('object');
   }
